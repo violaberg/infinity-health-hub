d15812cc3f365a46f428e7f56fdbe914e37dfa1f
Eddie Stan <eduard.eduard25@yahoo.com>
2024-04-23 23:01:31 +0100

    Create views, urls, templates for articles and posts

--- forum/urls.py
+++ forum/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.forum, name='forum'),
+    path('articles/', views.article_list, name='articles'),
+    path('posts/', views.post_list, name='posts'),
+    path('articles/<slug:slug>', views.article_detail, name='article_detail'),
+    path('posts/<slug:slug>', views.post_detail, name='post_detail'),
+
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Article, Post
 
-# Create your views here.
+
 def forum(request):
      
     return render(request, 'forum/forum.html')
+
+
+def article_list(request):
+    articles = Article.objects.filter(is_approved=True)
+    return render(request, 'forum/article_list.html', {'articles': articles})
+
+def post_list(request):
+    posts = Post.objects.filter(is_approved=True)
+    return render(request, 'forum/post_list.html', {'posts': posts})
+
+def article_detail(request, slug):
+    article = get_object_or_404(Article, slug=slug)
+    return render(request, 'forum/article_detail.html', {'article': article})
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, 'forum/post_detail.html', {'post': post})
