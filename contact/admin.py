eb04e78b9b161cb90c6615b0332facaffc55fd98
lukecdev <courtney.luke@outlook.com>
2024-04-24 07:51:26 +0000

    contact form, model and template created

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    """
+    A model admin to allow admin to view contact form submissions
+    """
+
+    list_display = (
+        'name',
+        'email',
+        'subject',
+        'message',
+    )
+    list_filter = ('name', 'email', 'subject',)
+    search_fields = ('name', 'email', 'subject',)
---
+++ contact/forms.py
@@ -0,0 +1,21 @@
+from django.forms import ModelForm
+from .models import Contact
+
+class ContactForm(ModelForm):
+    """
+    A form to allow users to contact the site owner
+    """
+    class Meta:
+        model = Contact
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+         super().__init__(*args, **kwargs)
+         placeholders = {
+            'email': 'Email',
+            
+            'message': 'Message',
+            
+        }
+
+         self.fields['email'].widget.attrs['autofocus'] = True
