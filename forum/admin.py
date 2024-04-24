719115f50eda351bcd552c3cb89c1001930f6fdb
Eddie Stan <eduard.eduard25@yahoo.com>
2024-04-24 19:45:05 +0100

    Create comment model,form, post_detail view and template

--- forum/admin.py
+++ forum/admin.py
@@ -1,17 +1,25 @@
 from django.contrib import admin
-from .models import Article, Post
+from .models import Article, Post, Reply
+from django_summernote.admin import SummernoteModelAdmin
 
-class ArticleAdmin(admin.ModelAdmin):
+
+class ArticleAdmin(SummernoteModelAdmin):
     list_display = ('title', 'author', 'created_on', 'is_approved')
     list_filter = ('is_approved', 'life_stage', 'neurodiversity')
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content')
 
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
     list_display = ('title', 'author', 'created_on', 'is_approved')
     list_filter = ('is_approved',)
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content')
+
+class ReplyAdmin(admin.ModelAdmin):
+    list_display = ('author', 'body')
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Post, PostAdmin)
+admin.site.register(Reply, ReplyAdmin)
