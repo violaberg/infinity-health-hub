306dca3f19ff03f413c689e7bae831c86c4886d3
Deirdre McCarthy <deirdremc1968@gmail.com>
2024-04-25 12:23:04 +0000

    bug: cannot signup #65

--- infinity_hub/settings.py
+++ infinity_hub/settings.py
@@ -46,6 +46,7 @@ CSRF_TRUSTED_ORIGINS = [
                         + 'ws-eu110.gitpod.io'),
                         'https://8000-vasileios20-infinityhub-n7uovthffb0.ws-eu110.gitpod.io',
                         'https://8000-vasileios20-infinityhub-n1cualn8mni.ws-eu110.gitpod.io',
+                        'https://8000-vasileios20-infinityhub-qyq3ge7a31m.ws-eu110.gitpod.io',
                        ]
 
 
@@ -93,9 +94,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'infinity_hub.urls'
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 TEMPLATES = [
     {
