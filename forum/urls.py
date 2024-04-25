64d2321989b2d3b5bdece50dd9b56b5a6d0a48ef
Eddie Stan <eduard.eduard25@yahoo.com>
2024-04-25 21:58:00 +0100

    Create functionality for liking and saving posts

--- forum/urls.py
+++ forum/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('posts/<int:post_id>/edit/', views.edit_post, name='edit_post'),
     path('posts/<int:post_id>/delete/', views.delete_post, name='delete_post'),
     path('posts/create/', views.create_post, name='create_post'),
+    path('posts/<int:post_id>/like/', views.like_post, name='like_post'),
+    path('posts/<int:post_id>/save/', views.save_post, name='save_post'),
 ]
